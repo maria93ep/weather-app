56b3896ec3386d75fbbb268e60d9efefa74aa5d5
Maria S <stanmaria9327@yahoo.com>
2023-11-26 16:20:06 +0200

    exemple

--- Desktop/it school/js functions/scripts.js
+++ Desktop/it school/js functions/scripts.js
@@ -21,19 +21,123 @@
   //  } 
  
    
-  function calculator(num1, num2, operator) {
- switch (operator) {
-  case "+":
-    return num1 + num2;
-  case "-":
-    return num1 - num2;
-   case "*":
-    return num1 * num2;
-  case "/":
-  return num1 / num2;
- default:
-   return "Invalid operator";}
-  }
+  //function calculator(num1, num2, operator) {
+ //switch (operator) {
+  //case "+":
+  //  return num1 + num2;
+  //case "-":
+  //  return num1 - num2;
+   //case "*":
+  //  return num1 * num2;
+  //case "/":
+  //return num1 / num2;
+ //default:
+ //  return "Invalid operator";}
+//  }
   
+  //objects exemples.
+
+   //const firstBook = {
+    //id: 1,
+    //title: "Harry Potter ",
+    //date: "15.11.1990",
+    //getThis: function(){
+      //console.table(this);
+   //},
+    //getBookDetails: function(){
+     //return console.log(
+       // "Book with ID:" + this.id + "and title: " + this.title + "date");
+      //},
+    //};
+
+  //const secondBook = {
+  //id: 2,
+   //title: "Back to the future",
+   //date: "15.11.1990",
+   //readingTime:120,
+   //arrowFnDetails: () => {
+  //console.log(this.title);
+  //},
+//updateReadingTime: function(newTime) {
+  ///this.readingTime = newTime;
+  //console.log('Calling updateReadingTime, that now is:',this.readingTime);
+ 
+ //},
+  //read :function() {
+    //console.log('Calling read');
+    //this.updateReadingTime(150);
+  //},
+//};
+  //normalFnDetails: function() {
+  //console.log(this.title)
+//},
+  //};
+//callInsideFn: function() {
+  //console.log('Calling callInsideFn');
+  //this.normalFnDetails()
+//}
+  //};
+  
+  // const theOtherBook = {
+    //id: 1,
+    //title: 'Harry Potter ',
+    //date: '15.11.1990'
+   //};
    
+   //const person1 = {
+    //name:"Eva",
+    //age: 2,
+   // city:"Iasi",
+    
+   // displayInfo: function(){
+     // console.log(`Name: {this.name}, Age: {this.age},City: {this.city}`);
+   // },
+  //};
   
+  //const person2 = {
+    //name: "Ira",
+     //age :3,
+    // city:"Orhei",
+    
+   //  display: function() {
+  //console.log(`Name: {this.name}, Age: {this.age}, City: {this.city}`);
+  //   },
+  //};
+  //22.11.2023
+  //Ex 1 (Data Types, Operators, Arrays):
+//Scrie un program care primește de la utilizator trei numere și le adaugă într-un array. Apoi, calculează suma numerelor din array și o afișează în consolă.
+  //const numbers = [];
+  //let sum = 0;
+  //for (let i = 0; i < numbers.length; i++) {
+  //}
+  //console.log(`Sum of array values: ${sum}`);
+   
+ //  Ex 3 (Switch, Functions, Arrays):
+//Scrie un program care primește un cod de țară de la utilizator și afișează numele țării respective, împreună cu o listă de capitale asociate. Folosește un obiect pentru a mapa codurile de țară la numele țărilor și la array-uri cu capitalele corespunzătoare.
+
+//const countryCode = ['RO','DE','UA'];
+//let countryName;
+//let countryCapital;
+
+//switch (countryCode) {
+  //case "RO":
+  //  countryName = "Romania";
+ // case "RO":
+ //   countryCapital = "Bucharest";
+  //  break;
+ // case "DE":
+   // countryName = "Germania";
+   //  case "DE":
+   // countryCapital = "Berlin";
+   // break;
+  //case "UA":
+  //  countryName = "Ukraina";
+   //  case "UA":
+  //  countryCapital = "Kiev";
+  //  break;
+ // default:
+  //  countryName = "Moldova";
+  //  break;
+//}
+//console.log(`The country with code ${countryCode} is ${countryName} ${countryCapital}`); var elementDiv = document.createElement("div");
+ 
